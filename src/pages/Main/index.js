31bff5265ee911381ee66198b1be8e92eea24200
Leandro Braga <ldrbraga@gmail.com>
2020-03-05 12:53:23 -0300

    Add state and save input data

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,7 +5,25 @@ import { FaJedi, FaSearch } from 'react-icons/fa';
 import { Container, Form, SelectOptions, SubmitButton } from './styles';
 
 export default class Main extends Component {
+  state = {
+    newSearch: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ newSearch: e.target.value });
+
+    console.log(this.state.newSearch);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state.newSearch);
+  }
+
   render() {
+    const { newSearch } = this.state;
+
     return (
       <Container>
         <h1>
@@ -13,15 +31,20 @@ export default class Main extends Component {
           Guia Star Wars
         </h1>
 
-        <Form onSubmit={() => {}}>
-          <input type="text" placeholder="Pesquisar" />
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            value={newSearch}
+            onChange={this.handleInputChange}
+          />
 
           <SelectOptions>
             <option value="movie">Filme</option>
             <option value="people">Personagem</option>
           </SelectOptions>
 
-          <SubmitButton disabled>
+          <SubmitButton>
             <FaSearch color="##fff" size={14} />
           </SubmitButton>
         </Form>
