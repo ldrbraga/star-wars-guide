4c6a69c3af8879bc1c8edd8d2d7d91eaca45865f
Leandro Braga <ldrbraga@gmail.com>
2020-03-05 12:06:29 -0300

    Add components and style to the main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaJedi, FaSearch } from 'react-icons/fa';
+
+import { Container, Form, SelectOptions, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Guia Star Wars</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaJedi />
+        Guia Star Wars
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Pesquisar" />
+
+        <SelectOptions>
+          <option value="movie">Filme</option>
+          <option value="people">Personagem</option>
+        </SelectOptions>
+
+        <SubmitButton disabled>
+          <FaSearch color="##fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,63 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #ffd700;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(255, 255, 255, 0.6);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 32px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-right: 10px;
+      color: #eb1c29;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SelectOptions = styled.select`
+  background: #fff;
+  border: 1px solid #eee;
+  padding: 10px 15px;
+  border-radius: 4px;
+  font-size: 16px;
+  margin: 0 10px;
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #eb1c29;
+  border: 0;
+  padding: 0 15px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    color: #fff;
+  }
 `;
