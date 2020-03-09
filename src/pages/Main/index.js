4bd6a08a9f7782203673718cc38c95c325c9b264
Leandro Braga <ldrbraga@gmail.com>
2020-03-09 10:20:07 -0300

    Implementing main component functionality

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
+import { FaJedi, FaSearch, FaSpinner } from 'react-icons/fa';
 
-import { FaJedi, FaSearch } from 'react-icons/fa';
+import api from '../../services/api';
 
 import { Container, Form, SelectOptions, SubmitButton } from './styles';
 
 export default class Main extends Component {
   state = {
-    newSearch: '',
+    search: '',
+    select: 'films',
+    loading: false,
   };
 
   handleInputChange = e => {
-    this.setState({ newSearch: e.target.value });
+    e.preventDefault();
+    this.setState({ search: e.target.value });
 
-    console.log(this.state.newSearch);
+    console.log(this.state.search);
   };
 
-  handleSubmit = e => {
+  handleSelectChange = async e => {
+    const { select } = this.state;
+    this.setState({ select: e.target.value });
+
+    console.log({select});
+  };
+
+  handleSubmit = async e => {
     e.preventDefault();
 
-    console.log(this.state.newSearch);
+    this.setState({ loading: true });
+
+    const { search } = this.state;
+
+    const response = await api.get(`${search}`);
+
+    console.log(response.data.results);
+
+    this.setState({
+      search: '',
+      loading: false,
+    });
   }
 
   render() {
-    const { newSearch } = this.state;
+    const { search, select, loading } = this.state;
 
     return (
       <Container>
@@ -35,17 +57,18 @@ export default class Main extends Component {
           <input
             type="text"
             placeholder="Pesquisar"
-            value={newSearch}
+            value={search}
             onChange={this.handleInputChange}
           />
 
-          <SelectOptions>
-            <option value="movie">Filme</option>
+          <SelectOptions onChange={this.handleSelectChange} value={select}>
+            <option value="films">Filme</option>
             <option value="people">Personagem</option>
+            <div>{`testando seleção: ${select}`}</div>
           </SelectOptions>
 
-          <SubmitButton>
-            <FaSearch color="#fff" size={14} />
+          <SubmitButton loading={loading}>
+            { loading ? (<FaSpinner color="#fff" size={14} />) : (<FaSearch color="#fff" size={14} />) }
           </SubmitButton>
         </Form>
       </Container>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -45,9 +45,20 @@ export const SelectOptions = styled.select`
   margin: 0 10px;
 `;
 
-export const SubmitButton = styled.button.attrs({
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-})`
+  disabled: props.loading,
+}))`
   background: #eb1c29;
   border: 0;
   padding: 0 15px;
@@ -56,4 +67,17 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
 `;
